91c20515e76f34c8adead9ad2902dc7e79c7fb54
625alex <625alex@gmail.com>
2014-07-23 13:55:34 -0600

    Update UVMonitor.groovy
    Added configurable frequency and UV reporting thresholds.

--- apps/UVMonitor.groovy
+++ apps/UVMonitor.groovy
@@ -19,8 +19,20 @@ definition(
     author: "Alex Malikov",
     description: "Notify when UV index rises to dangerous levels above 5.",
     category: "Health & Wellness",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/SafetyAndSecurity/App-MindYourHome.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/SafetyAndSecurity/App-MindYourHome@2x.png")
+
+preferences {
+	section("Check how often (minutes)?") {
+		input name: "frequency", title: "Frequency", type: "number", defaultValue: 15
+	}
+    section ("Only report when UV is above...") {
+    	input name: "reportMinimum", type: "number", defaultValue: 5
+    }
+    section ("Only report when UV changes by...") {
+    	input name: "delta", type: "number", defaultValue: 1
+    }
+}
 
 
 def installed() {
@@ -33,41 +45,63 @@ def updated() {
 }
 
 def initialize() {
+	log.debug "settings: $settings"
 	checkUV()
-	schedule("0 */30 * * * ?", "checkUV")
 }
 
 def getUv() {
-	getWeatherFeature("conditions")?.current_observation?.UV as Integer
+	def cond = getWeatherFeature("conditions")?.current_observation
+	def uv = cond?.UV as Integer
+    uv = uv ?: 0
+    if (uv < 0) uv = 0
+    
+    def tf = new java.text.SimpleDateFormat("h:mm a")
+    log.debug "TZ offcet: ${cond?.local_tz_offset}"
+    tf.setTimeZone(TimeZone.getTimeZone("GMT${cond?.local_tz_offset ?: 0}"))
+    def lastRun = "${tf.format(new Date())}"
+    
+    state.lastRun = lastRun
+    
+    log.debug "uv: $uv"
+    uv
 }
 
 def checkUV() {
 	def uv = getUv()
-    log.debug "checkUv: $uv"
-    if (uv && uv > 0) {
-    	if (state.lastUv != uv) {
-	        def risk
-	        if (uv >= 11) {
-	            risk = "Extreame"
-	        } else if (uv >= 8) {
-	        	risk = "Very high"
-	        } else if (uv >= 6) {
-	        	risk = "High"
-	        }
-	        
-	        if (uv > 5) {
-	        	sendPush("UV is $uv. " + risk + " risk of UV exposure.")
-	        } else {
-	        	if (state.risk) {
-	            	sendPush("UV is $uv or lower.")
-	            }
-	        }
-	        
-	        state.risk = risk
-	        
-	    }
-	state.lastUv = uv
-    } 
     
-    state.lastRun = new Date().toSystemFormat()
+    if (state.lastUv != uv) {
+        def risk
+        if (uv >= 11) {
+            risk = "Extreme"
+        } else if (uv >= 8) {
+        	risk = "Very high"
+        } else if (uv >= 6) {
+        	risk = "High"
+        } else if (uv >= 3) {
+        	risk = "Moderate"
+        } else if (uv >= 1) {
+        	risk = "Low"
+        } else {
+        	risk = "No"
+        }
+        
+        state.risk = risk
+        
+        if (uv >= reportMinimum) {
+        	if (Math.abs(state.lastUv ?: 0 - uv) >= delta) {
+            	def message = "UV index at ${location.name} is $uv. " + risk + " risk of UV exposure."
+            	sendPush(message)
+            	log.debug message
+                state.isLowReported = false
+            }
+        } else if (!state.isLowReported) {
+            sendPush("UV is $uv or lower.")
+            state.isLowReported = true
+        }
+        
+    }
+    
+    state.lastUv = uv
+    
+    runIn(frequency * 60, checkUV, [overwrite: false])
 }
